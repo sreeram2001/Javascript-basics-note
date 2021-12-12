a56cbf478667dd1f6b444b5e0760bf6f61cd6995
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-12-12 13:08:05 +0530

    Add files via upload

---
+++ Introduction/Loops/loops.js
@@ -0,0 +1,30 @@
+
+
+//for-loop
+
+for(let s=0;s<5;s++) {
+    console.log(s);
+}
+
+
+
+//while - loop
+
+let i = 5;
+
+while(i<10) {
+    console.log(i);
+    i++;
+}
+
+
+
+//do-while loop
+
+let it = 10;
+
+do {
+    console.log(it);
+    it++;
+}
+while(it<15);
