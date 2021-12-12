7e21041d1c32ef4c364510b8a62ab209577da0ea
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-12-12 11:51:01 +0530

    Add files via upload

---
+++ Introduction/Conditional-Statements/condition.js
@@ -0,0 +1,15 @@
+
+let hour = 10;
+
+if(hour >= 6 && hour <12) 
+    console.log('Good Morning');
+
+else if(hour >=12 && hour < 4) 
+    console.log('Good Afternoon');
+
+else if(hour >= 4 && hour < 7)  
+    console.log('Good Evening');
+
+else 
+    console.log('Good Night');
+
