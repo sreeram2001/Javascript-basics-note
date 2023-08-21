32718de5878ad568566df63f10195d573fceeb5e
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-08-22 00:36:08 +0530

    Create index.js

---
+++ ES6_CONCEPTS/Modules And Modularity/index.js
@@ -0,0 +1,9 @@
+//this file was created for implementing modules and modularity
+
+//const calculator holds all functions and modules of calculator.js file
+const calculator = require('./calculator');
+
+calculator.addition(2,3)
+calculator.sub(2,3)
+calculator.mult(2,3)
+calculator.div(2,3)
