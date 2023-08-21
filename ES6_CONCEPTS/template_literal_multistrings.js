0480373853d07a24b19784bbfcaf93b52848e238
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-08-22 00:29:09 +0530

    Add files via upload

---
+++ ES6_CONCEPTS/template_literal_multistrings.js
@@ -0,0 +1,9 @@
+//thru the following way we can write multiline strings without concatenation
+console.log(`Hi 
+there I am 
+Sreeram`)
+
+
+//we can use variables or objects like this without concatenation
+let height = 165
+console.log(`my height is ${height} cms.`)
