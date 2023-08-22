f189aeba3faefbe64c9a1f85be28f30260496007
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-08-22 21:30:14 +0530

    Add files via upload

---
+++ Other Concepts/truthy_falsy.js
@@ -0,0 +1,17 @@
+//in JAvascript for every value there is a boolean value
+let a = ""
+
+if(a)
+{
+    console.log("truthy")
+}
+else{
+    console.log("falsy")
+}
+
+//it returns false for the following
+//Nan - not a number
+//null
+//empty string or character '', "" //no space in between
+//0 -0
+//undefined
