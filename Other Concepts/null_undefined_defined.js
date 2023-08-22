f69afde4caf479306e2dd34ae19c1f9e3d6cc71d
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-08-22 21:15:32 +0530

    Add files via upload

---
+++ Other Concepts/null_undefined_defined.js
@@ -0,0 +1,20 @@
+//undefined
+let a;
+console.log(a);
+
+//undefined means value has not been assigned, by default also undefined
+let arr = [];
+console.log(arr[0])
+
+function sum(x,y)
+{
+    let sumi = x+y;
+}
+
+pp = sum(2,3)
+console.log(pp) //undefined as there is not return value
+
+
+//NULL is just an explicit value that we assign to vraibles or objects or anything
+let myname = null
+console.log(myname)
---
+++ Other Concepts/primitive_datatypes.js
@@ -0,0 +1,22 @@
+//in this example we are using primitve data types, we assign value of first to second
+//and change second but first remains same
+let first = 'a'
+let second = first
+
+first = 'b'
+console.log("first : ", first)
+console.log("second : ", second)
+
+
+//in this example we are using an object, we assign a to b and change one parameter it gets 
+//changed in b as well, this is reference data type
+var a = {
+    name : "sree"
+}
+var b = a;
+a.name = "ram"
+console.log(a,b)
+
+
+//in primitive data types we use stack memory, in reference data types we use heap memeory
+//values are fixed in stack memory
